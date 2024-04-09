5fb052a64bec251cf1e1e42c2164a2ebe9d4760b
Doug Scheible <doug_scheible@yahoo.com>
2024-04-09 18:18:19 -0500

    changed port access

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const api = require('./routes/index.js');
 
 // Require the JSON file and assign it to a variable called `termData`
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
