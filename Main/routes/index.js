b66b0b102752a0e48a152d5158fee3bdf01c7185
Doug Scheible <doug_scheible@yahoo.com>
2024-04-09 20:40:17 -0500

    cleaned up file structure - heroku working now

--- Main/routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-// Import our modular routers for /notes
-
-const notesRouter = require('./notes');
-
-router.use('/notes', notesRouter);
-
-module.exports = router;
